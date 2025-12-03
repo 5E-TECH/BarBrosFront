fdb5b532a103f3263b538f03cc33bd66cb238aa8
Bahodir Nabijanov <bahodirnabijanov782@gmail.com>
2025-12-03 17:02:25 +0500

    login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,74 @@
-import { memo } from 'react';
+import { memo, useState } from "react";
+import { Eye, EyeClosed } from "lucide-react";
+import lock from "../../shared/assets/Lock.png";
 
 const Login = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center w-[600px] rounded-md shadow-2xl py-[50px]">
+        <div className="px-[100px]">
+          <div className="bg-[#F8F8F8] rounded-full flex justify-center items-center mb-[37px] w-[250px] h-[250px] mx-auto">
+            <img
+              src={lock}
+              alt="Lock icon"
+              className="object-contain w-[100px] h-[100px]"
+            />
+          </div>
+
+          <h1 className="text-[28px] font-medium text-maintext">Kirish</h1>
+          <div>
+            <div className="flex flex-col mb-[27px]">
+              <label
+                htmlFor=""
+                className="text-helpertext font-normal text-[14px] flex justify-between mb-[6px]"
+              >
+                Login
+              </label>
+              <div className="border border-[#E8E9EB] rounded-2xl flex justify-between px-[16px] py-[10px]">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="enter name"
+                  className="outline-0"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col mb-[27px]">
+              <label
+                htmlFor=""
+                className="text-helpertext font-normal text-[14px] flex justify-between mb-[6px]"
+              >
+                Parol
+              </label>
+              <div className="border border-[#E8E9EB] rounded-2xl flex justify-between px-[16px] py-[10px]">
+                <input
+                  type={show ? "text" : "password"}
+                  name=""
+                  id=""
+                  placeholder="enter password"
+                  className="outline-0"
+                />
+                <button
+                  onClick={() => setShow(!show)}
+                  className="cursor-pointer"
+                >
+                  {show ? (
+                    <EyeClosed size={20} color="#3F434A" />
+                  ) : (
+                    <Eye size={20} color="#3F434A" />
+                  )}
+                </button>
+              </div>
+            </div>
+            <button className="w-full rounded-[7px] py-[8px] font-medium text-[15px] text-white cursor-pointer bg-main hover:bg-mainhover">
+              Kirish
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
